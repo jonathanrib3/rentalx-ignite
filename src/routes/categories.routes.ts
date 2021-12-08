848ee363474797e850cb98fa0766bda34d1faba5
jonathanrib3 <jonathanrib.3@gmail.com>
2021-12-08 18:29:26 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,23 @@
+import { Request, Response, Router } from "express";
+
+import CategoriesRepository from "../repositories/CategoriesRepository";
+import CreateCategoryService from "../services/CreateCategoryService";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.get("/", (request: Request, response: Response) => {
+  return response.status(200).send(categoriesRepository.list());
+});
+
+categoriesRoutes.post("/", (request: Request, response: Response) => {
+  const { name, description } = request.body;
+
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export default categoriesRoutes;
